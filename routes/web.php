2733851d840c141319ae94daea81cb8656c52efc
Rafieh <59613343+Rafieh1377@users.noreply.github.com>
2020-01-09 08:12:18 +0330

    added files

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+	
+});
+
+Route::get('/user/{id}', function ($id) {
+    
+	return 'Hello '.$id.' ! How are you ?';
+});
+
+ 
+Route:: post('/signup',[
+    'uses' => 'UserController@postSignUp',
+    'as' => 'signup'
+    ]);
+
+Route:: post('/signin',[
+    'uses' => 'UserController@postSignIn',
+    'as' => 'signin'
+    ]);
+     
+
+Route::get('/dashboard', [
+    'uses' => 'UserController@getDashboard',
+    'as' => 'dashboard'
+]);
+?>
+    
+
+
+
+
